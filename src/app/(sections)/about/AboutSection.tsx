ce7ad2d3830c246ee5e193b8c73384744cf39c0f
Niklas Ladehoff <business-mail-niklas@web.de>
2023-04-22 03:07:57 +0200

    added content to about page

--- src/app/(sections)/about/AboutSection.tsx
+++ src/app/(sections)/about/AboutSection.tsx
@@ -2,24 +2,106 @@ import React from "react";
 
 import "./../../../styles/about.css";
 
+const zilla_bold = Zilla_Slab({ subsets: ["latin"], weight: "700" });
+
+const zilla_semi = Zilla_Slab({ subsets: ["latin"], weight: "600" });
+
+import { Zilla_Slab } from "next/font/google";
+import SmallCloud from "@/components/SmallCloud";
+
 export default function AboutSection() {
   return (
     <section className="relative min-h-screen h-screen w-screen bg-[url('../../public/assets/background-portfolio-about.png')] bg-center bg-no-repeat bg-cover">
-      <div className="w-screen overflow-x-hidden">
+      <div className="w-screen overflow-x-hidden h-12 overflow-y-clip absolute top">
         <svg
+          className=""
           width="3000"
           height="300"
           viewBox="0 0 2000 200"
-          className=""
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="m0 87 12.3-.8c12.4-.9 37-2.5 61.7-3.4 24.7-.8 49.3-.8 74 4.4 24.7 5.1 49.3 15.5 74 12 24.7-3.5 49.3-20.9 74-28.2 24.7-7.3 49.3-4.7 74 6.8 24.7 11.5 49.3 31.9 74.2 42.5 24.8 10.7 49.8 11.7 74.6-.5 24.9-12.1 49.5-37.5 74.2-57.6C617.7 42 642.3 27 667 24.8c24.7-2.1 49.3 8.5 74 30.2 24.7 21.7 49.3 54.3 74 58.5 24.7 4.2 49.3-20.2 74-33.8 24.7-13.7 49.3-16.7 74-7.5 24.7 9.1 49.3 30.5 74 45.5 24.7 15 49.3 23.6 74 6.5 24.7-17.2 49.3-60.2 74-64.9 24.7-4.6 49.3 29 74 36.4 24.7 7.3 49.3-11.7 74-10 24.7 1.6 49.3 24 74 38.1 24.7 14.2 49.3 20.2 74.2 5.5 24.8-14.6 49.8-50 74.6-61.1 24.9-11.2 49.5 1.8 74.2 10.5 24.7 8.6 49.3 13 74 25.5 24.7 12.5 49.3 33.1 74 39.5 24.7 6.3 49.3-1.7 74-14.7 24.7-13 49.3-31 74-44.8 24.7-13.9 49.3-23.5 61.7-28.4L2000 51V0H0Z"
+            d="m0 18 12.8-1c12.9-1 38.5-3 64.2-1 25.7 2 51.3 8 77 7.3 25.7-.6 51.3-8 77-8.6 25.7-.7 51.3 5.3 77 5.8 25.7.5 51.3-4.5 77-6 25.7-1.5 51.3.5 76.8 1.3 25.5.9 50.9.5 76.4-.5 25.5-1 51.1-2.6 76.8-3 25.7-.3 51.3.7 77 .9 25.7.1 51.3-.5 77 2.6C794.7 19 820.3 26 846 29c25.7 3 51.3 2 77 1.2 25.7-.9 51.3-1.5 77-4.2 25.7-2.7 51.3-7.3 77-10.7 25.7-3.3 51.3-5.3 77-6.5 25.7-1.1 51.3-1.5 77 .7 25.7 2.2 51.3 6.8 77 11.8 25.7 5 51.3 10.4 77 7.9 25.7-2.5 51.3-12.9 76.8-13.5 25.5-.7 50.9 8.3 76.4 8.6 25.5.4 51.1-8 76.8-12.3 25.7-4.3 51.3-4.7 77 0 25.7 4.7 51.3 14.3 77 17.3 25.7 3 51.3-.6 77-5.3 25.7-4.7 51.3-10.3 77-9.5 25.7.8 51.3 8.2 64.2 11.8L2000 30V0H0Z"
             fill="#0e0e0e"
           />
         </svg>
       </div>
-      <div className="w-screen overflow-x-hidden absolute bottom-0">
+      <div className="text-white z-10 flex flex-row justify-between h-full pt-8 pb-32 ">
+        <div className="w-1/2 flex flex-row justify-center items-center">
+          <div className="bg-primary rounded-3xl border-2 border-black h-[43rem] w-[40rem] p-6 flex flex-col gap-5">
+            <h1 className={`${zilla_bold.className} text-3xl`}>Who Am I...</h1>
+            <p className={`${zilla_semi.className} text-xl text-sectxtcolor`}>
+              I am Niklas, right now 17 years old, a fullstack developer with a
+              love for typescript and python. My first contact with software
+              development was when i was around 10 years old. Back then I
+              created plugins with java but I left that behind quickly.
+            </p>
+            <p className={`${zilla_semi.className} text-xl text-sectxtcolor`}>
+              After I distanced myself from programming for a while, I picked it
+              back up at 13. This time it got me! The first language I learned
+              back then was python. I developed small programs like calculators,
+              small games and managment systems. After I got confident with my
+              tools, I heard about machine learning and got right into it and
+              yes it was hard.
+            </p>
+            <p className={`${zilla_semi.className} text-xl text-sectxtcolor`}>
+              My first big project I picked up was self driving car with a
+              raspberry pi, self build car, opencv, self build car, and a self
+              trained neural network. I created that project for a school
+              presentation about autonomous driving.
+            </p>
+            <p className={`${zilla_semi.className} text-xl text-sectxtcolor`}>
+              This all brought me to todays point. Now I am creating fullstack
+              application and keep learning more! Python sticked to me as a
+              backend language, but I added new tools to my backpack like
+              typescipt, react, nextjs, django, tailwind, flutter, dart and
+              more.
+            </p>
+          </div>
+        </div>
+        <div className="w-1/2 pr-24 py-8 flex flex-col gap-16">
+          <div className="flex flex-row justify-start">
+            <div className="relative ">
+              <SmallCloud classes="scale-[1.2]" />
+              <div className="absolute top-0 h-full w-full  flex flex-row justify-center items-center">
+                <h3 className={`${zilla_bold.className} z-10 text-3xl `}>
+                  Teamplayer
+                </h3>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row justify-between">
+            <div className="relative ">
+              <SmallCloud classes="scale-70" />
+              <div className="absolute top-0 h-full w-full  flex flex-row justify-center items-center">
+                <h3 className={`${zilla_bold.className} z-10 text-3xl `}>
+                  ...
+                </h3>
+              </div>
+            </div>
+            <div className="relative ">
+              <SmallCloud classes="scale-[1.6]" />
+              <div className="absolute top-0 h-full w-full  flex flex-row justify-center items-center">
+                <h3 className={`${zilla_bold.className} z-10 text-3xl `}>
+                  Creative
+                </h3>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row justify-center">
+            <div className="relative ">
+              <SmallCloud classes="scale-[1.4]" />
+              <div className="absolute top-0 h-full w-full  flex flex-row justify-center items-center">
+                <h3 className={`${zilla_bold.className} z-10 text-3xl `}>
+                  Active Learner
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-screen overflow-x-hidden absolute bottom-0 ">
         <svg
           className=" "
           width="3000"
@@ -33,7 +115,6 @@ export default function AboutSection() {
           />
         </svg>
       </div>
-      <div className="flex flex-row items-center pl-0 sm:pl-10 md:pl-28 lg:pl-28 xl:pl-72 text-white h-full w-full "></div>
     </section>
   );
 }
---
+++ src/components/SmallCloud.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+export default function SmallCloud({ classes }: { classes: string }) {
+  return (
+    <svg
+      className={`${classes} -z-10`}
+      xmlns="http://www.w3.org/2000/svg"
+      width={248}
+      height={189}
+      fill="none"
+    >
+      <g filter="url(#a)">
+        <path
+          fill="#2C2C2C"
+          fillRule="evenodd"
+          d="M165.587 29.202c-4.503 0-8.784.976-12.655 2.734C147.806 21.306 137.147 14 124.828 14c-10.307 0-19.452 5.115-25.167 13.012-6.016-3.867-13.125-6.102-20.742-6.102-21.645 0-39.191 18.047-39.191 40.308 0 4.442.698 8.715 1.988 12.711a36.493 36.493 0 0 0-1.764-.042C19.544 73.887 3 90.902 3 111.892c0 20.989 16.544 38.005 36.952 38.005 2.864 0 5.653-.335 8.33-.97.438 19.431 15.88 35.053 34.88 35.073 12.305.013 23.13-6.52 29.361-16.403 5.763 9.822 16.238 16.389 28.206 16.389 12.162 0 22.784-6.783 28.484-16.873a26.648 26.648 0 0 0 14.738 4.435c9.13 0 17.215-4.606 22.176-11.683a26.644 26.644 0 0 0 14.551 4.313c15.09 0 27.322-12.582 27.322-28.101 0-15.52-12.232-28.101-27.322-28.101-.359 0-.718.007-1.074.021.41-1.936.626-3.947.626-6.01 0-14.969-11.38-27.204-25.727-28.053a32.946 32.946 0 0 0 2.437-12.485c0-17.81-14.037-32.247-31.353-32.247Z"
+          clipRule="evenodd"
+        />
+      </g>
+      <g filter="url(#b)">
+        <path
+          fill="#2C2C2C"
+          fillRule="evenodd"
+          d="M115.198 36.112c16.706 0 30.425 13.157 31.895 29.945l.354-.002c17.687 0 32.025 14.747 32.025 32.938 0 17.519-13.299 31.844-30.08 32.878-5.684 9.176-15.665 15.262-27.027 15.262a31.183 31.183 0 0 1-15.157-3.916c-7.454 5.921-16.8 9.444-26.946 9.444-24.365 0-44.118-20.316-44.118-45.376s19.753-45.376 44.118-45.376c1.224 0 2.436.052 3.634.152 3.116-14.834 15.947-25.949 31.302-25.949Z"
+          clipRule="evenodd"
+        />
+      </g>
+      <defs>
+        <filter
+          id="a"
+          width={275}
+          height={195}
+          x={3}
+          y={-11}
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feColorMatrix
+            in="SourceAlpha"
+            result="hardAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          />
+          <feOffset dx={30} dy={-25} />
+          <feGaussianBlur stdDeviation={15.5} />
+          <feComposite in2="hardAlpha" k2={-1} k3={1} operator="arithmetic" />
+          <feColorMatrix values="0 0 0 0 0.116667 0 0 0 0 0.116667 0 0 0 0 0.116667 0 0 0 1 0" />
+          <feBlend in2="shape" result="effect1_innerShadow_207_409" />
+        </filter>
+        <filter
+          id="b"
+          width={215.327}
+          height={188.549}
+          x={0.144}
+          y={0.112}
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feColorMatrix
+            in="SourceAlpha"
+            result="hardAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          />
+          <feOffset dx={30} dy={-25} />
+          <feGaussianBlur stdDeviation={30} />
+          <feComposite in2="hardAlpha" k2={-1} k3={1} operator="arithmetic" />
+          <feColorMatrix values="0 0 0 0 0.0833333 0 0 0 0 0.0833333 0 0 0 0 0.0833333 0 0 0 1 0" />
+          <feBlend in2="shape" result="effect1_innerShadow_207_409" />
+          <feGaussianBlur
+            result="effect2_foregroundBlur_207_409"
+            stdDeviation={18}
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}

b95ac8a14d28f95a738cd644c8e23e290d72e2af
Niklas Ladehoff <business-mail-niklas@web.de>
2023-04-23 15:16:56 +0200

    navbar to left side

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -47,37 +47,35 @@ export default function NavBar() {
   }, []);
 
   return (
-    <div className="flex justify-center absolute w-screen h-screen bg-red-500">
-      <nav ref={navNarRef} className="bg-primary z-10 fixed rounded-md ">
-        <ul className="flex  flex-row gap-4 md:gap-8 text-base md:text-xl py-4 px-4 md:px-6 text-txtcolor">
-          <button
-            className={`${zilla.className} scale-100 hover:scale-95 hover:cursor-pointer  transition-transform hover:duration-200`}
-          >
-            Home
-          </button>
-          <button
-            className={`${zilla.className} scale-100 hover:scale-95 hover:cursor-pointer  transition-transform hover:duration-200`}
-          >
-            About
-          </button>
-          <button
-            className={`${zilla.className} scale-100 hover:scale-95 hover:cursor-pointer transition-transform hover:duration-200`}
-          >
-            Skills
-          </button>
-          <button
-            className={`${zilla.className} scale-100 hover:scale-95 hover:cursor-pointer transition-transform hover:duration-200`}
-          >
-            Projects
-          </button>
-          <button
-            className={`${zilla.className} scale-100 hover:scale-95 hover:cursor-pointer transition-transform hover:duration-200`}
-          >
-            Contact
-          </button>
-        </ul>
-      </nav>
-    </div>
+    <nav ref={navNarRef} className="bg-primary z-10 fixed rounded-md ">
+      <ul className="flex  flex-row gap-4 md:gap-8 text-base md:text-xl py-4 px-4 md:px-6 text-txtcolor">
+        <button
+          className={`${zilla.className} scale-100 hover:scale-95 hover:cursor-pointer  transition-transform hover:duration-200`}
+        >
+          Home
+        </button>
+        <button
+          className={`${zilla.className} scale-100 hover:scale-95 hover:cursor-pointer  transition-transform hover:duration-200`}
+        >
+          About
+        </button>
+        <button
+          className={`${zilla.className} scale-100 hover:scale-95 hover:cursor-pointer transition-transform hover:duration-200`}
+        >
+          Skills
+        </button>
+        <button
+          className={`${zilla.className} scale-100 hover:scale-95 hover:cursor-pointer transition-transform hover:duration-200`}
+        >
+          Projects
+        </button>
+        <button
+          className={`${zilla.className} scale-100 hover:scale-95 hover:cursor-pointer transition-transform hover:duration-200`}
+        >
+          Contact
+        </button>
+      </ul>
+    </nav>
   );
 }
 

e737187d8be23ddb84635a71006618df4c67cb2c
Niklas Ladehoff <business-mail-niklas@web.de>
2023-04-26 20:26:45 +0200

    removed bg image

--- src/app/(sections)/skills/SkillsSection.tsx
+++ src/app/(sections)/skills/SkillsSection.tsx
@@ -3,8 +3,6 @@ import React from "react";
 
 export default function SkillsSection() {
   return (
-    <section className="relative min-h-screen h-screen w-screen">
-      <div className="flex flex-row items-center pl-0 sm:pl-10 md:pl-28 lg:pl-28 xl:pl-72 text-white h-full w-full "></div>
-    </section>
+    <section className="relative xl:min-h-screen min-h-fit w-screen bg-[url('../../public/assets/skills-bg.png')] bg-center bg-repeat-y bg-cover "></section>
   );
 }
